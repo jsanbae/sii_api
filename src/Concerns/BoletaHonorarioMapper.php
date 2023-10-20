f5afc4ddce39895d1965c3b281f352b219a9948a
Javier Sanchez <jsan5709@hotmail.com>
2023-10-20 11:08:22 -0300

    fix(bhe): emisor mapping

--- src/Concerns/BoletaHonorarioMapper.php
+++ src/Concerns/BoletaHonorarioMapper.php
@@ -16,11 +16,13 @@ trait BoletaHonorarioMapper {
         return $boletas;
     }
 
+    // Unify data from BHE and BTE
     public function mapBoleta($boleta):BoletaHonorario 
     {        
         $nro_boleta = isset($boleta['nro_boleta']) ? $boleta['nro_boleta'] : $boleta['nroboleta'];
-        $rut_emisor = isset($boleta['rut_emisor']) ? explode('-',$boleta['rut_emisor'])[0] : $boleta['rutemisor'];
-        $dv_emisor = isset($boleta['rut_emisor']) ? explode('-',$boleta['rut_emisor'])[1] : $boleta['dvemisor'];
+        $nombre_emisor = isset($boleta['receptor_nombre']) ? $boleta['receptor_nombre'] : $boleta['nombre_emisor'];
+        $rut_emisor = isset($boleta['receptor_rut']) ? explode('-',$boleta['receptor_rut'])[0] : $boleta['rutemisor'];
+        $dv_emisor = isset($boleta['receptor_rut']) ? explode('-',$boleta['receptor_rut'])[1] : $boleta['dvemisor'];
         $bruto = isset($boleta['bruto']) ? $boleta['bruto'] : $boleta['totalhonorarios'];
         $retenido = isset($boleta['retenido']) ? $boleta['retenido'] : $boleta['retencion_receptor'];
         $pagado = isset($boleta['pagado']) ? $boleta['pagado'] : $boleta['honorariosliquidos'];
@@ -30,7 +32,7 @@ trait BoletaHonorarioMapper {
             $boleta['estado'],
             $rut_emisor,
             $dv_emisor,
-            $boleta['nombre_emisor'],
+            $nombre_emisor,
             $boleta['fecha_boleta'],
             $bruto,
             $retenido,
